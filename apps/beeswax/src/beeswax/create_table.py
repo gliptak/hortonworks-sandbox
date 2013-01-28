81860d7a9006a741c1e5b3d97c66b373f0b49a59
hortonworks-sandbox <pramod@hortonworks.com>
2013-01-28 11:01:41 -0800

    Update apps/beeswax/src/beeswax/create_table.py

--- apps/beeswax/src/beeswax/create_table.py
+++ apps/beeswax/src/beeswax/create_table.py
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#  Portions Copyright © 2013 Hortonworks, Inc.
+
 """
 Views & controls for creating tables
 """
