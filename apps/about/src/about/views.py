365c097d18852036d949fa15f9b54b13280ee1bd
hortonworks-sandbox <pramod@hortonworks.com>
2013-01-28 10:58:27 -0800

    Update apps/about/src/about/views.py

--- apps/about/src/about/views.py
+++ apps/about/src/about/views.py
@@ -14,6 +14,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Portions Copyright © 2013 Hortonworks, Inc.
 
 from django.conf import settings
 from desktop.lib.django_util import render
